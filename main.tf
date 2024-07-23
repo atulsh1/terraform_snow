b9bb2104148311aa7638755a50248976c2c3d555
Atul Sharma <atul.sharma@gmail.com>
2024-07-23 18:21:18 +0530

    added tf code

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Define the Snowflake database, use lifecycle to prevent changes if it already exists
+# Define the Snowflake database
 resource "snowflake_database" "NW_DBA1" {
   name                     = "NW_DBA1"
   data_retention_time_in_days = 3
@@ -8,7 +8,7 @@ resource "snowflake_database" "NW_DBA1" {
   }
 }
 
-# Define the Snowflake schema, add a count to prevent errors if it already exists
+# Define the Snowflake schema
 resource "snowflake_schema" "file_schema" {
   count              = 1
   database           = snowflake_database.NW_DBA1.name
@@ -38,7 +38,7 @@ resource "snowflake_database_grant" "db_grant" {
 # Grant privileges to the roles on the schema
 resource "snowflake_schema_grant" "sch_grant" {
   database_name      = snowflake_database.NW_DBA1.name
-  schema_name        = snowflake_schema.file_schema.name
+  schema_name        = snowflake_schema.file_schema[0].name
   privilege          = "USAGE"
   roles              = [snowflake_role.role_developer.name]
   with_grant_option  = false
