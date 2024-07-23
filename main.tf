3b62dff90358e808758027345558f32531cff514
Atul Sharma <atul.sharma@gmail.com>
2024-07-23 16:59:09 +0530

    added tf code

--- main.tf
+++ main.tf
@@ -1,36 +1,35 @@
 resource "snowflake_database" "NW_DBA1" {
-  name = "NW_DBA1"
+  name                     = "NW_DBA1"
   data_retention_time_in_days = 3
 }
 
 resource "snowflake_schema" "file_schema" {
-    database = "NW_DBA1"
-    name     = "CUSTOMER_SCHEMAS_ID"
-    is_transient        = false
-    is_managed          = false
-    data_retention_days = 1
+  database           = snowflake_database.NW_DBA1.name
+  name               = "CUSTOMER_SCHEMA"
+  is_transient       = false
+  is_managed         = false
+  data_retention_days = 1
 }
 
-resource "snowflake_role" "role_01" {
+resource "snowflake_role" "role_developer" {
   name = "DEVELOPER"
 }
 
 resource "snowflake_role" "role_data_analyst" {
-  name = "DATA_ANALYSTS"
+  name = "DATA_ANALYST"
 }
 
 resource "snowflake_database_grant" "db_grant" {
-    database_name = "NW_DBA1"
-    privilege = "USAGE"
-    roles = [ "Analyst" ]
-    with_grant_option = false
+  database_name      = snowflake_database.NW_DBA1.name
+  privilege          = "USAGE"
+  roles              = [snowflake_role.role_data_analyst.name]
+  with_grant_option  = false
 }
 
 resource "snowflake_schema_grant" "sch_grant" {
-    database_name = "NW_DBA1"
-    schema_name = snowflake_schema.file_schema.name
-    privilege = "USAGE"
-    roles = [ "DEVELOPERS" ]
-    with_grant_option = false
+  database_name      = snowflake_database.NW_DBA1.name
+  schema_name        = snowflake_schema.file_schema.name
+  privilege          = "USAGE"
+  roles              = [snowflake_role.role_developer.name]
+  with_grant_option  = false
 }
-
