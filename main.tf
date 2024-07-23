36ee5606da1543cdcebdecc9056fb19a8b783384
Atul Sharma <atul.sharma@gmail.com>
2024-07-23 16:47:19 +0530

    added github action environment

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "snowflake_database" "NW_DBA1" {
-  name = "NW_DBA2"
+  name = "NW_DBA1"
   data_retention_time_in_days = 3
 }
 
 resource "snowflake_schema" "file_schema" {
-    database = "NW_DBA2"
-    name     = "CUSTOMER_SCHEMA"
+    database = "NW_DBA1"
+    name     = "CUSTOMER_SCHEMASs"
     is_transient        = false
     is_managed          = false
     data_retention_days = 1
@@ -20,17 +20,17 @@ resource "snowflake_role" "role_data_analyst" {
 }
 
 resource "snowflake_database_grant" "db_grant" {
-    database_name = "NW_DBA2"
+    database_name = "NW_DBA1"
     privilege = "USAGE"
-    roles = [ "DEVELOPER" ]
+    roles = [ "DEVELOPERS" ]
     with_grant_option = false
 }
 
 resource "snowflake_schema_grant" "sch_grant" {
-    database_name = "NW_DBA2"
+    database_name = "NW_DBA1"
     schema_name = snowflake_schema.file_schema.name
     privilege = "USAGE"
-    roles = [ "DEVELOPER" ]
+    roles = [ "DEVELOPERS" ]
     with_grant_option = false
 }
 
